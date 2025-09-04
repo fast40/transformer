3a4f5926c3c96fb269ea125dd91b897151dac443
fast40 <eclfast@gmail.com>
2025-09-03 17:04:53 -0700

    Some progress on tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -16,5 +16,9 @@ pair_counts = Counter(zip(chars, chars[1:]))
 
 pair_substitutions = []
 
-for i in range(1000):
-# print(pair_counts)
+# for i in range(1000):
+pair = max(Counter(zip(chars, chars[1:])))
+# print(Counter(zip(chars, chars[1:])))
+print('\n'.join(f'{chr(key[0])}{chr(key[1])}: {value}' for key, value in sorted(Counter(zip(chars, chars[1:])).items(), key=lambda x: x[1])))
+print(''.join(chr(item) for item in pair))
+    # take the most common pairing and replace it
